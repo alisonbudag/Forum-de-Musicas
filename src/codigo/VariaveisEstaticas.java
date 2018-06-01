5e1547c8df20c159362420f5e8056e67d63bc5d9
ALISONRAFAEL <ALISONRAFAEL@DESKTOP-18687FL.mshome.net>
2018-06-01 08:25:22 -0300

    Adicionando Classe para variáveis estáticas

---
+++ src/codigo/VariaveisEstaticas.java
@@ -0,0 +1,12 @@
+package codigo;
+
+public class VariaveisEstaticas {
+	
+	//Ao logar no sistema
+	public static String usuarioLogado;
+	public static boolean isMod;
+	public static boolean isAdm;
+	
+	
+
+}
